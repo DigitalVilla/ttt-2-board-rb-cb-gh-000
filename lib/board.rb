29c0577c00171e05f42dbf27272ffb3b51ce7732
Omar Villa <o.villanueva@thearcane.com>
2020-06-14 01:44:38 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,6 @@
-# Define the variable board below.
-tile =  " "
-count = 9
-board = (tile*count).split('')
+# Define the variable board below.tile =  " "
+board = set_board(" ",9)
+
+def set_board(tile, count)
+   return (tile*count).split('')
+end
